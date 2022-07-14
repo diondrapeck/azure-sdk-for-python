0abe49f8b138c2da12eb483ff1d9968582d04550
Diondra Peck <16376603+diondrapeck@users.noreply.github.com>
2022-07-13 18:08:15 -0700

    Update ml folder

--- sdk/ml/azure-ai-ml/azure/ai/ml/identity/__init__.py
+++
@@ -1,7 +0,0 @@
-# ---------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# ---------------------------------------------------------
-
-from ._credentials import AzureMLOnBehalfOfCredential
-
-__all__ = ["AzureMLOnBehalfOfCredential"]
--- sdk/ml/azure-ai-ml/tests/test_configs/deployments/endpoint_scoring/do_nothing.py
+++
@@ -1,5 +0,0 @@
-def init() -> None:
-    pass
-
-def run(raw_data: list) -> list:
-    return [{"result": "Hello World"}] 
